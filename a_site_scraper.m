20742556f141e1703e70d0a46d010a7ebd7d1f9f
Hayato Tenryuu <hayatotenryuu@gmail.com>
2024-06-27 14:08:25 -0500

    Added data fetcher and began an analytic section

---
+++ a_site_scraper.m
@@ -0,0 +1,23 @@
+er = 0;
+for u = 0:35
+    try
+        name = "Powerball_Year_" + int2str(u) + ".html";
+        site = "https://www.powerball.net/archive/" + int2str(u+1992);
+        stroo = webread(site);
+        locale = dir;
+        last = size(locale);
+        finder = locale(last).name;
+
+        if finder ~= "data"
+            mkdir data          
+        end
+        
+        fid = fopen("data\" + name, "w+");
+        nbytes = fprintf(fid, stroo);
+        fclose(fid);
+
+    catch exception
+        er = er + 1;
+        fprintf("this is error number " + er + newline);
+    end
+end
---
+++ b_data_organizer.m
@@ -0,0 +1,6 @@
+for u = 0:32
+    
+    fid = fopen("Powerball_Year_" + u + ".html");
+    fclose fid;
+
+end
