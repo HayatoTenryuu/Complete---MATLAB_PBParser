961dc58430b6027ef82579b6ca948554e79962b2
Hayato Tenryuu <hayatotenryuu@gmail.com>
2024-06-28 17:10:25 -0500

    Added comments and date-finding functionality

--- b_data_organizer.m
+++ b_data_organizer.m
@@ -1,10 +1,12 @@
 %% This file organizes the data and exports excel files.
 
+% Setup
 cd .\data;
 a = dir;
 datnum = length(a) - 3;
 cd ..;
 
+% Initialize arrays
 dateArray = {};
 b1Array = {};
 b2Array = {};
@@ -17,27 +19,30 @@ for u = 0:datnum
     
     try 
 
-        fid = fopen("data\Powerball_Year_" + int2str(u) + ".html", "r");        % Open as read-only so you don't make the data files empty.
+        fid = fopen("data\Powerball_Year_" + int2str(u) + ".html", "r");        % Open as read-only so you don't make the data files empty
+        contents = fileread("data\Powerball_Year_" + int2str(u) + ".html");     % Import the file's text
+        
+        search_line_a = '<a class="archive-box" href="/numbers/';               % Date keyword search
+        search_line_b = '<div class="ball">';                                   % White Ball keyword search
+        search_line_c = '<div class="powerball">';                              % Red Ball keyword search
 
-        %---------------------------------------------------------------
-        % Insert functionality to pull this info from the HTML
-        Date = bla;
-        B1 = bla;
-        B2 = bla;
-        B3 = bla;
-        B4 = bla;
-        B5 = bla;
-        RB = bla;
-        %---------------------------------------------------------------
+        date_points = strfind(contents, search_line_a);                         % Find the location/index of each date
+        wb_points = strfind(contents, search_line_b);                           % Find the location/index of each white ball number
+        rb_points = strfind(contents, search_line_c);                           % Find the location/index of each red ball number
 
-        dateArray = [dateArray, Date];
-        b1Array = [b1Array, B1];
-        b2Array = [b2Array, B2];
-        b3Array = [b3Array, B3];
-        b4Array = [b4Array, B4];
-        b5Array = [b5Array, B5];
-        rbArray = [rbArray, RB];
+        % Find values of each variable and assign it to arrays
+        for v = date_points(1):length(date_points)
+       
+            dateArray = [dateArray, contents(v+length(search_line_a)):contents(v+length(search_line_a)+10)];
+            b1Array = [b1Array, bla];
+            b2Array = [b2Array, bla];
+            b3Array = [b3Array, bla];
+            b4Array = [b4Array, bla];
+            b5Array = [b5Array, bla];
+            rbArray = [rbArray, bla];
 
+        end
+        
         fclose(fid);
         
     catch err
