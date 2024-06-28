0f0ff1474e2f0a1f9d4268629ae41043d10765c8
Hayato Tenryuu <hayatotenryuu@gmail.com>
2024-06-28 14:12:12 -0500

    Better formatting and comments

--- a_site_scraper.m
+++ a_site_scraper.m
@@ -1,23 +1,35 @@
 er = 0;
+
 for u = 0:35        % This represents 36 years of Powerball data, should return errors. If not, increase year count.
+    
     try
-        name = "Powerball_Year_" + int2str(u) + ".html";
+
+        % Pull each year's data from powerball website
         site = "https://www.powerball.net/archive/" + int2str(u+1992);
         stroo = webread(site);
+        
+        % Save that year's data as an HTML file.
         locale = dir;
         last = size(locale);
         finder = locale(last).name;
 
         if finder ~= "data"
-            mkdir data          
+            mkdir data         
         end
-        
-        fid = fopen("data\" + name, "w+");
+
+        nama = "Powerball_Year_" + int2str(u) + ".html";
+        fid = fopen("data\" + nama, "w+");
         nbytes = fprintf(fid, stroo);
         fclose(fid);
 
     catch exception
+        
+        % Intentionally produce an error message as proof that we are
+        % grabbing all of the data. If there are more errors than expected,
+        % we have an actual problem.
         er = er + 1;
         fprintf("this is error number " + er + newline);
+
     end
+
 end
